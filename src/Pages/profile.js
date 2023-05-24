3ae78a1b181ee31524ce8ea1be45249cb5538606
YUVARAJ1992 <yuvarajatravi@gmail.com>
2023-05-24 12:31:15 +0530

    updated profile

--- src/Pages/profile.js
+++ src/Pages/profile.js
@@ -1,17 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ProfilePage = () => {
+
+  const [profilelist, updateProfiles] = useState({ firstname: "", lastname: "" });
+  const [userProfiles, updateUserProfiles] = useState([]);
+
+  const getProfiles = (event) => {
+    console.log(event.target.value)
+    updateProfiles({ ...profilelist, [event.target.name]: event.target.value })
+  }
+
+  const clickButton = () => {
+    console.log(profilelist)
+    updateUserProfiles([...userProfiles, profilelist])
+    console.log(userProfiles);
+  }
+
+  const ClicktoDelete = (number) => {
+    let copy = [...userProfiles];
+    copy = copy.filter(
+      (item, index) => number != index
+    )
+    updateUserProfiles(copy)
+  }
+
   return (
     <div>
       <h3>Profile Infomation</h3>
       <div>
         <label>Enter First Name :</label>
-        <input type='text' placeholder='First Name' />
+        <input type='text' placeholder='First Name' onChange={getProfiles} name='firstname' />
 
         <label>Enter Last Name :</label>
-        <input type='text' placeholder='Last Name' />
+        <input type='text' placeholder='Last Name' onChange={getProfiles} name='lastname' />
 
-        <button>Sumbit</button>
+        <button onClick={() => clickButton()}>Sumbit</button>
       </div>
       <table>
         <thead>
@@ -22,13 +45,19 @@ const ProfilePage = () => {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Milk Moch</td>
-            <td>
-              <button>Update</button>
-              <button>Delete</button>
-            </td>
-          </tr>
+          {
+            userProfiles.map((value, index) => {
+              return (
+                <tr key={index}>
+                  <td>{value.firstname} {value.lastname}</td>
+                  <td>
+                    <button>Update</button>
+                    <button onClick={() => ClicktoDelete(index)}>Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
         </tbody>
       </table>
     </div>
